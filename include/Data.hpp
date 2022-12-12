e801cd6779fe504dbd9aa82d875e3409d53b0a49
qvan_le <vanle199tk@gmail.com>
2022-12-12 09:20:48 +0700

    format and add namespace GraphicsData

--- include/Data.hpp
+++ include/Data.hpp
@@ -6,9 +6,34 @@
 #include <iostream>
 #include <iomanip>
 
-namespace Data
+using CellType = int;
+using Cell = std::array<int, 2>;
+
+namespace BoardData
 {
+    //  declare constant
+    extern const CellType SafeCells[];
+    extern const CellType FlagCell;
+    extern const CellType QuestionCell;
+    extern const CellType MineCell;
+
+
+    //  variable
+    extern std::vector<CellType> Board;
+    extern int Columns, Rows;
+    extern int Mines;
+
+    extern std::vector<std::vector<int> > Components;
+    extern std::vector<int> ComponentID;
     void init(int n, int m, int k);
     void init();
     void show();
+}
+
+
+namespace GraphicsData
+{
+    extern std::vector<sf::Texture> icons;
+    extern const int Width;
+    extern const int Height;
 }
