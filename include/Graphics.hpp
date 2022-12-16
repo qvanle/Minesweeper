659cd57e7a0979ceb061eeb7bfbfe05761540101
qvan_le <vanle199tk@gmail.com>
2022-12-16 15:07:03 +0700

    add starting screen

--- include/Graphics.hpp
+++ include/Graphics.hpp
@@ -2,5 +2,26 @@
 
 namespace Graphics
 {
+    class Button
+    {
+    private:
+        sf::Texture image;
+        sf::Vector2f position;
+    public:
+        int getW();
+        int getH();
+        Button();
+        Button(std::string link);
+        sf::Sprite getSprite();
+        sf::Sprite getSprite(float x, float y);
+        void setImage(std::string);
+    };
+
+    namespace StartingScreen
+    {
+        void Draw();
+    }
+
+    void INIT();
     void RunGraphics();
 }
